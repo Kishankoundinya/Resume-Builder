a3db543a4a05ada24283f8c693437c1e24f2295e
Kishan Koundinya <kishankoundinya31@gmail.com>
2026-08-15 16:35:07 +0530

    preview section added frontend working succesfully

--- client/src/pages/ResumeBuilder.jsx
+++ client/src/pages/ResumeBuilder.jsx
@@ -64,12 +64,16 @@ const ResumeBuilder = () => {
     const frontendUrl=window.location.href.split('/app/')[0];
     const resumeUrl=frontendUrl+'/view/'+resumeId;
 
-    if(navigator.share){
-      navigator.share({url: resumeUrl,text:"My Resume",})
-    }else{
-      alert('Share not supported on this browser.')
+     if (navigator.share) {
+        navigator.share({
+            title: "My Resume",
+            url: resumeUrl,
+        });
+    } else {
+        navigator.clipboard.writeText(resumeUrl);
+        alert("Resume link copied!");
     }
-  }
+};
 
 
   const downloadResume=()=>{
