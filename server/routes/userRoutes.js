b6517bd0ad022749408f71e2a768c824947c684e
Kishan Koundinya <kishankoundinya31@gmail.com>
2026-08-16 14:43:34 +0530

    resume.js added

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -6,8 +6,8 @@ import protect from "../middlewares/authMiddleware.js";
 const userRouter=express.Router();
 
 userRouter.post('/register',registerUser);
-userRouter.post('/login',loginUserUser);
-userRouter.get('/data',protect,getUserByIdUser);
+userRouter.post('/login',loginUser);
+userRouter.get('/data',protect,getUserById);
 userRouter.get('/resumes',protect,getUserResume)
 
 
